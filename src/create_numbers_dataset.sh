8aefde353a0aa2dca37d9364e62d56a3a6b561e0
Wiktor Flis <wikflis@gmail.com>
2022-09-15 18:05:35 +0200

    Create working train/test split automatic script

--- src/create_numbers_dataset.sh
+++ src/create_numbers_dataset.sh
@@ -7,6 +7,6 @@ fi
 # for loop from 0 to 9 inclusive
 for i in ({0..9}); do
     #cp -r ../data/all_characters/$i ../data/numbers/$i
-    echo "../data/all_characters/$i"
+    echo "../data/all_characters_raw/$i"
 done
 
