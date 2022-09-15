6cd3da470eb79f1b2620be5210145000ff695436
Wiktor Flis <wikflis@gmail.com>
2022-09-15 18:05:35 +0200

    Create working train/test split automatic script

--- src/preprocessing/create_numbers_dataset.py
+++ src/preprocessing/create_numbers_dataset.py
@@ -9,20 +9,13 @@ from utils import create_train_test_sets
 current_path = pathlib.Path(__file__).resolve().parent
 
 # check if ../../data/numbers exists
-target_path = pathlib.Path(current_path / "../../data/numbers/train+test")
+target_path = pathlib.Path(current_path / "../../data/numbers")
 target_path.mkdir(parents=True, exist_ok=True)
 
 classes_dir = [str(i) for i in range(10)]
 test_ratio = 0.2
 
-for cls in classes_dir:
-    create_train_test_sets(current_path / "../../data/numbers" / cls, target_path / cls, test_ratio)
-
 for i in classes_dir:
-    pathlib.Path(current_path / "../../data/numbers/" / classes_dir).mkdir(
-        parents=True, exist_ok=True
-    )
-    for file in pathlib.Path(current_path / "../../data/all_characters" / classes_dir).glob(
-        "*.*"
-    ):
-        shutil.copy(file, target_path / classes_dir / file.name)
+    print("Creating train & test set for class: {}".format(i))
+    create_train_test_sets(current_path / "../../data/all_characters" / i, target_path, test_ratio)
+print("Done")
--- src/preprocessing/utils.py
+++ src/preprocessing/utils.py
@@ -8,6 +8,14 @@ def train_test_split(data: list, test_ratio: float = 0.2):
     return np.split(np.array(data), [int(len(data) * (1 - test_ratio))])
 
 def create_train_test_sets(path: str, target_path: str, test_ratio: float = 0.2):
+    '''
+    Takes a path to a directory containing images and splits them into train and test sets.
+
+    Example:
+
+    Source path: src_path/0
+    Target path: target_path/0/train and target_path/0/test
+    '''
     current_path = pathlib.Path(path).resolve()
     target_path = pathlib.Path(target_path)
     target_path.mkdir(parents=True, exist_ok=True)
@@ -21,10 +29,13 @@ def create_train_test_sets(path: str, target_path: str, test_ratio: float = 0.2)
     # split the list into train and test
     train_filenames, test_filenames = train_test_split(all_filenames, test_ratio)
 
+    print("Total images found: ", len(all_filenames))
+    print(f"Will be split using ratio {test_ratio} into \n\t{len(train_filenames)} train and \n\t{len(test_filenames)} test images\n")
+
+
     # create train and test directories in target_path
     (target_path / "train").mkdir(parents=True, exist_ok=True)
     (target_path / "test").mkdir(parents=True, exist_ok=True)
-
     # copy files to train and test directories
     for name in train_filenames:
         shutil.copy(name, target_path / "train" / name.name)
