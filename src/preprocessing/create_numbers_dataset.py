659c5dd1c6b989e9f037ebb33b9fbbac671ca8d1
Wiktor Flis <wikflis@gmail.com>
2022-09-15 18:11:44 +0200

    Fix train/test split automatic script

--- src/preprocessing/create_numbers_dataset.py
+++ src/preprocessing/create_numbers_dataset.py
@@ -17,5 +17,5 @@ test_ratio = 0.2
 
 for i in classes_dir:
     print("Creating train & test set for class: {}".format(i))
-    create_train_test_sets(current_path / "../../data/all_characters" / i, target_path, test_ratio)
+    create_train_test_sets(current_path / "../../data/all_characters" / i, target_path, class_name=i, test_ratio=test_ratio)
 print("Done")
