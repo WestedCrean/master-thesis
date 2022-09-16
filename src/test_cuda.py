3aaf6ef2f378e23a6523dd1bb174c859a1aafcdd
Wiktor Flis <wikflis@gmail.com>
2022-09-16 14:51:44 +0200

    Add cuda availability test

---
+++ src/test_cuda.py
@@ -0,0 +1,13 @@
+import torch
+import tensorflow as tf
+
+print("CHECKING FOR PYTORCH")
+if torch.cuda.is_available():    
+    print('There are %d GPU(s) available.' % torch.cuda.device_count())
+    print('We will use the GPU:', torch.cuda.get_device_name(0))
+
+else:
+    print('No GPU available, using the CPU instead.')
+   
+print("\n\nCHECKING FOR TENSORFLOW")
+print(tf.config.list_physical_devices('GPU'))
