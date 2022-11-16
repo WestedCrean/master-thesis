a26e3ee76c1edeb61c0011f81a8e8e53302a8550
Wiktor Flis <wikflis@gmail.com>
2022-11-16 17:05:17 +0200

    Add VGG model architecture

--- src/training/create_models.py
+++ src/training/create_models.py
@@ -52,6 +52,91 @@ def get_convnet_model(
     return model, config
 
 
+def get_vgg_model(
+    input_shape: tuple,
+    num_classes: int,
+    learning_rate: float,
+    model_name: str,
+    optimizer: tf.keras.optimizers.Optimizer = tf.keras.optimizers.Adam,
+    loss: tf.keras.losses.Loss = tf.keras.losses.CategoricalCrossentropy(),
+    metrics: list = [tf.keras.metrics.CategoricalAccuracy()],
+) -> Tuple[tf.keras.Model, dict]:
+    # Input layer
+    inputs = tf.keras.Input(shape=input_shape)
+    x = inputs
+
+    # Convolutional layers
+    x = tf.keras.layers.Conv2D(
+        filters=64, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.Conv2D(
+        filters=64, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(x)
+    x = tf.keras.layers.Conv2D(
+        filters=128, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.Conv2D(
+        filters=128, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(x)
+    x = tf.keras.layers.Conv2D(
+        filters=256, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.Conv2D(
+        filters=256, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.Conv2D(
+        filters=256, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(x)
+    x = tf.keras.layers.Conv2D(
+        filters=512, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.Conv2D(
+        filters=512, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.Conv2D(
+        filters=512, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(x)
+    x = tf.keras.layers.Conv2D(
+        filters=512, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.Conv2D(
+        filters=512, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.Conv2D(
+        filters=512, kernel_size=(3, 3), padding="same", activation="relu"
+    )(x)
+    x = tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2))(x)
+
+    # Flatten
+    x = tf.keras.layers.Flatten()(x)
+
+    # Dense layers
+    x = tf.keras.layers.Dense(units=4096, activation="relu")(x)
+    x = tf.keras.layers.Dense(units=4096, activation="relu")(x)
+
+    # Output layer
+    outputs = tf.keras.layers.Dense(num_classes, activation="softmax")(x)
+    # Create model
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    # Compile model
+    model.compile(
+        optimizer=optimizer(learning_rate=learning_rate),
+        loss=loss,
+        metrics=metrics,
+    )
+
+    config = dict(
+        learning_rate=learning_rate,
+        model_name=model_name,
+    )
+
+    return model, config
+
+
 def get_models_for_experiment(num_classes=10, input_shape=(32, 32, 3)) -> List:
     """
     Create models with different hyperparameters to be trained from scratch
