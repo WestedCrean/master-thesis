1585e058f5ae170e882a25106bda862a84f28e66
Wiktor Flis <wikflis@gmail.com>
2023-01-04 19:48:07 +0100

    Fix wandb artifact?

--- src/wandb/upload_raw_dataset.py
+++ src/wandb/upload_raw_dataset.py
@@ -7,7 +7,35 @@ import numpy as np
 from loguru import logger
 import wandb
 
+def upload_raw_dataset():
+    base_dir = pathlib.Path().resolve().parent
+    raw_data_source = f"{base_dir}/data/all_characters"
 
+    labels = [str(i) for i in range(0, 9)]
+    run = wandb.init(project="master-thesis", job_type="upload")
+
+    data_at = wandb.Artifact(f"numbers", type=f"raw_data")
+    
+    for l in labels:
+        imgs_per_label = os.path.join(raw_data_source, l)
+        if os.path.isdir(imgs_per_label):
+            # filter out "DS_Store"
+            imgs = [i for i in os.listdir(imgs_per_label) if not i.startswith(".DS")]
+            # randomize the order
+            shuffle(imgs)
+            print(f"Number of images for label {l}: {len(imgs)}")
+            img_file_ids = imgs
+            for f in img_file_ids:
+                file_path = os.path.join(raw_data_source, l, f)
+                print(f"File path: {file_path}")
+                # add file to artifact by full path
+                data_at.add_file(file_path, name=l + "/" + f)
+
+    print("Adding artifact to run")
+    run.log_artifact(data_at)
+    run.finish()
+
+'''
 def upload_raw_dataset():
     """
     Uploads a dataset of handwritten latin letters with diacritics from the PHCD dataset to Weights & Biases.
@@ -18,7 +46,7 @@ def upload_raw_dataset():
     #labels = [str(i) for i in range(10, 80)]
     labels = [str(i) for i in range(0, 9)]
     
-    run = wandb.init(project="master_thesis")
+    run = wandb.init(project="master-thesis", job_type="upload")
     #data_at = wandb.Artifact(f"latin_letters_with_diacritics", type=f"raw_data")
     data_at = wandb.Artifact(f"numbers", type=f"raw_data")
 
@@ -35,31 +63,7 @@ def upload_raw_dataset():
     run.log_artifact(data_at)
     run.finish()
     logger.info("Done") 
+'''
 
-#upload_raw_dataset()
-
-base_dir = "/home/wiktor/code/master-thesis"
-
-SRC = f"{base_dir}/data/all_characters"
-labels = [str(i) for i in range(0, 9)]
-run = wandb.init(project="mastethesis", job_type="upload")
-
-data_at = wandb.Artifact(f"numbers", type=f"raw_data")
- 
-for l in labels:
-  imgs_per_label = os.path.join(SRC, l)
-  print(imgs_per_label)
-  if os.path.isdir(imgs_per_label):
-    # filter out "DS_Store"
-    imgs = [i for i in os.listdir(imgs_per_label) if not i.startswith(".DS")]
-    # randomize the order
-    shuffle(imgs)
-    img_file_ids = imgs
-    for f in img_file_ids:
-      file_path = os.path.join(SRC, l, f)
-      # add file to artifact by full path
-      data_at.add_file(file_path, name=l + "/" + f)
-
-print("Adding artifact to run")
-run.log_artifact(data_at)
-run.finish()
+if __name__ == "__main__":
+    upload_raw_dataset()
