d9fd4fa31f0ab1f60acde4568490a3833ab0fc05
Wiktor Flis <wikflis@gmail.com>
2023-01-04 18:03:07 +0100

    Add script for uploading data to wandb

---
+++ src/wandb/upload_raw_dataset.py
@@ -0,0 +1,65 @@
+import os
+import sys
+from random import shuffle
+
+import pathlib
+import numpy as np
+from loguru import logger
+import wandb
+
+
+def upload_raw_dataset():
+    """
+    Uploads a dataset of handwritten latin letters with diacritics from the PHCD dataset to Weights & Biases.
+    Both lowercase and uppercase letters are included.
+    """
+
+    project_path = pathlib.Path().resolve().parent
+    #labels = [str(i) for i in range(10, 80)]
+    labels = [str(i) for i in range(0, 9)]
+    
+    run = wandb.init(project="master_thesis")
+    #data_at = wandb.Artifact(f"latin_letters_with_diacritics", type=f"raw_data")
+    data_at = wandb.Artifact(f"numbers", type=f"raw_data")
+
+    for l in labels:
+        source_path = pathlib.Path(
+            project_path / "data/all_characters" / l,
+        )
+        imgs = [i for i in source_path.iterdir()]
+        np.random.shuffle(imgs)
+        for img_path in imgs:
+            img_name = img_path.name
+            data_at.add_file(str(img_path), name=l + "/" + img_name)
+
+    run.log_artifact(data_at)
+    run.finish()
+    logger.info("Done") 
+
+#upload_raw_dataset()
+
+base_dir = "/home/wiktor/code/master-thesis"
+
+SRC = f"{base_dir}/data/all_characters"
+labels = [str(i) for i in range(0, 9)]
+run = wandb.init(project="mastethesis", job_type="upload")
+
+data_at = wandb.Artifact(f"numbers", type=f"raw_data")
+ 
+for l in labels:
+  imgs_per_label = os.path.join(SRC, l)
+  print(imgs_per_label)
+  if os.path.isdir(imgs_per_label):
+    # filter out "DS_Store"
+    imgs = [i for i in os.listdir(imgs_per_label) if not i.startswith(".DS")]
+    # randomize the order
+    shuffle(imgs)
+    img_file_ids = imgs
+    for f in img_file_ids:
+      file_path = os.path.join(SRC, l, f)
+      # add file to artifact by full path
+      data_at.add_file(file_path, name=l + "/" + f)
+
+print("Adding artifact to run")
+run.log_artifact(data_at)
+run.finish()
