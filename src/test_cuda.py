53f228635d85e6033017ffd281a4d683974c77d1
Wiktor Flis <wikflis@gmail.com>
2023-01-14 15:53:48 +0100

    Move to WSL

--- src/test_cuda.py
+++ src/test_cuda.py
@@ -1,14 +1,13 @@
 import torch
 import tensorflow as tf
-from loguru import logger
 
-logger.info("CHECKING FOR PYTORCH")
+print("CHECKING FOR PYTORCH")
 if torch.cuda.is_available():
-    logger.info("There are %d GPU(s) available." % torch.cuda.device_count())
-    logger.info("We will use the GPU:", torch.cuda.get_device_name(0))
+    print("There are %d GPU(s) available." % torch.cuda.device_count())
+    print("We will use the GPU:", torch.cuda.get_device_name(0))
 
 else:
-    logger.info("No GPU available, using the CPU instead.")
+    print("No GPU available, using the CPU instead.")
 
-logger.info("\n\nCHECKING FOR TENSORFLOW")
-logger.info(tf.config.list_physical_devices("GPU"))
+print("\n\nCHECKING FOR TENSORFLOW")
+print(tf.config.list_physical_devices("GPU"))
